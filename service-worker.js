fb9ca1dcda260004c6d770edeeb1109187724e68
ciredemets-ops <cire.demets@gmail.com>
2026-09-07 16:29:38 +0200

    V0.2: refresh offline cache and new assets

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,23 @@
-const CACHE='petanque-score-v01';
-const ASSETS=['./','./index.html','./manifest.json','./icons/icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const CACHE='petanque-score-v02';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon.svg'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
 self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(resp=>{
+      const copy=resp.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return resp;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
     const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
-  }).catch(()=>caches.match('./index.html'))));
+  })));
 });

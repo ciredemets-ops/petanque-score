43d59a002e855701ce1f5b2917e4a67c6e33ae0d
ciredemets-ops <cire.demets@gmail.com>
2026-09-08 00:08:20 +0200

    Refresh PWA cache for V0.4.7

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='petanque-score-v046';
+const CACHE='petanque-score-v047';
 const ASSETS=['./','./index.html','./styles.css','./manifest.json','./icons/icon.svg','./bg-marbre.svg'];
 
 self.addEventListener('install', event => {
